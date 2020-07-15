5e1413c8dd963f07666c451aa9efadcc3992187f
Lê Đức Thắng <leducthang98@gmail.com>
2020-07-16 00:38:24 +0700

    lesson01

---
+++ Lesson01/src/lesson01/Lesson01.java
@@ -0,0 +1,85 @@
+package lesson01;
+
+import java.util.Scanner;
+
+public class Lesson01 {
+
+    public static void main(String[] args) {
+//        int i = 5;
+//        int a = 6; //-2^32 - 2^32
+//        Integer x = new Integer(10);
+//        long b = 300; // -2^64 - 2^64
+//        double d = 3.12;
+//        boolean bool = true;
+//        Boolean bo = new Boolean(true);
+//        int j;
+//        j = 100;
+//        float c = 3.14f;
+//        Float z = new Float(3.123);
+//        char k = '!';
+//        System.out.println("d = " + (int) d);
+//        System.out.println("k = " + (int) k);
+//        String s = "Le Duc Thang";
+
+        // int, double, float, boolean, char, byte
+        // +, -, *, /, %
+//        int a = 8;
+//        int b = 3;
+//
+//        int result;
+//
+//        result = a % b;
+//        System.out.println("ket qua la:" + result);
+        //Scanner (doi tuong)
+        // cong cu ho tro nguoi dung nhap du lieu tu ban phim
+//        boolean bo;
+//        int a;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Moi ban nhap vao 1 so:");
+//
+//        int soVuaNhap;
+//
+//        soVuaNhap = sc.nextInt();
+//        System.out.println("So vua nhap la:" + soVuaNhap);
+        // ax + b = 0
+//        Scanner sc = new Scanner(System.in);
+//        int a, b;
+//        System.out.println("Nhap a:");
+//        a = sc.nextInt();
+//        System.out.println("Nhap b:");
+//        b = sc.nextInt();
+//
+//        if (a == 0) {
+//            if (b == 0) {
+//                System.out.println("Vo so nghiem");
+//            } else {
+//                System.out.println("Vo nghiem");
+//            }
+//        } else {
+//            //a != 0 va b != 0
+//            float result = -(float) b / a;
+//            System.out.println("Nghiem cua pt:" + result);
+//        }
+//        for (int i = 10; i > 0; i--) {
+//            System.out.println(i);
+//        }
+        // Yeu cau nguoi dung nhap vao 1 so nguyen tu 100-999999
+        // Neu so ma nguoi dung nhap vao ko thoa man thi bao loi
+        // Neu thoa man: In ra tat ca cac so nho hon so do, va chia het cho 3
+//        Scanner sc = new Scanner(System.in);
+//        int input;
+//        System.out.println("Moi nhap vao 1 so:");
+//        input = sc.nextInt(); //102 : 0 3 6 9 12 ,....
+//
+//        if (100 < input && input < 999999) {
+//
+//            for (int i = 0; i < input; i += 3) { //34 //102 
+//                System.out.println(i);
+//            }
+//
+//        } else {
+//            System.out.println("So nhap nam ngoai khoang 100-999999");
+//        }
+
+    }
+}
